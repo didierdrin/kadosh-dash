2d012b26cacca2afac791580176034d544619f17
Didier Nsengiyumva <nsedidier@gmail.com>
2024-07-10 19:42:06 +0200

    extra mod now

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['firebasestorage.googleapis.com']
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-          crypto: require.resolve('crypto-browserify'),
-          stream: require.resolve('stream-browserify'),
-          url: require.resolve('url/'),
-          zlib: require.resolve('browserify-zlib'),
-          http: require.resolve('stream-http'),
-          https: require.resolve('https-browserify'),
-          assert: require.resolve('assert/'),
-          os: require.resolve('os-browserify/browser'),
-        }
-      }
-      return config
-    },
-    // Add this section
-    experimental: {
-      esmExternals: 'loose'
-    },
-    // Add this section to suppress specific warnings
-    onDemandEntries: {
-      // period (in ms) where the server will keep pages in the buffer
-      maxInactiveAge: 25 * 1000,
-      // number of pages that should be kept simultaneously without being disposed
-      pagesBufferLength: 2,
-    },
-  }
+const nextConfig = {};
   
   export default nextConfig;
 
